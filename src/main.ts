606ee802ee658031a4ffbcbfa9e790e959876091
IanLiu2603 <ian.liu2603@gmail.com>
2024-10-08 21:37:19 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,17 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+//Counter
+let counterNum: number = 0;
+const counterText = document.createElement("div");
+counterText.innerHTML = `${counterNum} clicks`;
+app.append(counterText);
+
+//Button
 const middleButton = document.createElement("button");
 middleButton.innerHTML = "🐥";
+middleButton.onclick = function buttonClick() {
+  counterNum += 1;
+  counterText.innerHTML = `${counterNum} clicks`;
+};
 app.append(middleButton);
